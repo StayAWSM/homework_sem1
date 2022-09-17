8da158235ef577977e6677fb93d1e926493d74b9
StayAWSM <alex.babichev.95@gmail.com>
2022-09-17 23:54:43 +0300

    Закомментили, что забыли закомментить обратно

--- Program.cs
+++ Program.cs
@@ -91,31 +91,31 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-Console.Clear();
-Console.Write("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int it = 1;
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int it = 1;
 
-if (num < 1)
-{
-    Console.Write("Неверное число");
-}
-if (num == 1)
-{
-    Console.Write("Нет четных");
-}
-else while (it <= num)
-    {
-        if ((it % 2) == 0)
-        {
-            Console.WriteLine(it);
-            it = it + 1;
-        }
-        else if ((it % 2) == 1)
-        {
-            it = it + 1;
-        }
-    }
+// if (num < 1)
+// {
+//     Console.Write("Неверное число");
+// }
+// if (num == 1)
+// {
+//     Console.Write("Нет четных");
+// }
+// else while (it <= num)
+//     {
+//         if ((it % 2) == 0)
+//         {
+//             Console.WriteLine(it);
+//             it = it + 1;
+//         }
+//         else if ((it % 2) == 1)
+//         {
+//             it = it + 1;
+//         }
+//     }
 
 
 // // Вариант 2 (не готов еще)
